8d11fe45095d3d267d2ec73e3561b0d8a0e16ab8
comethalley <83149734+comethalley@users.noreply.github.com>
2023-05-17 21:31:27 +0800

    add chaeyoung, lia

--- src/Components/Main.jsx
+++ src/Components/Main.jsx
@@ -1,213 +1,298 @@
-import React from 'react'
- 
-import Card from '@mui/material/Card';
-import CardActions from '@mui/material/CardActions';
-import CardContent from '@mui/material/CardContent';
-import CardMedia from '@mui/material/CardMedia';
-import Button from '@mui/material/Button';
-import Typography from '@mui/material/Typography';
- 
+import React from "react";
+
+import Card from "@mui/material/Card";
+import CardActions from "@mui/material/CardActions";
+import CardContent from "@mui/material/CardContent";
+import CardMedia from "@mui/material/CardMedia";
+import Button from "@mui/material/Button";
+import Typography from "@mui/material/Typography";
+
 function Main() {
-  
- 
-  
   return (
     <>
-    <div className='container-card'>
-      <Card sx={{ maxWidth: 300 }}>
-      <CardMedia
-        sx={{ height: 200 }}
-        image="https://i.pinimg.com/564x/a8/4c/03/a84c037facbceae8def8b08484040ca7.jpg"
-        title="sana"
-      />
-      <CardContent>
-        <Typography gutterBottom variant="h5" component="div">
-          Minatozaki Sana
-        </Typography>
-        <Typography variant="body2" color="text.secondary">
-        Sana Minatozaki, known mononymously as Sana, is a Japanese 
-        singer based in South Korea. She is one of 
-        three Japanese members of the South Korean girl group Twice,
-         formed in 2015 by JYP Entertainment
-        </Typography>
-      </CardContent>
-      <CardActions>
-        <Button size="small">Share</Button>
-        <Button size="small">Learn More</Button>
-      </CardActions>
-    </Card>
+      <div className="container-card">
+        <Card sx={{ maxWidth: 300 }}>
+          <CardMedia
+            sx={{ height: 200 }}
+            image="https://i.pinimg.com/564x/a8/4c/03/a84c037facbceae8def8b08484040ca7.jpg"
+            title="sana"
+          />
+          <CardContent>
+            <Typography gutterBottom variant="h5" component="div">
+              Minatozaki Sana
+            </Typography>
+            <Typography variant="body2" color="text.secondary">
+              Sana Minatozaki, known mononymously as Sana, is a Japanese singer
+              based in South Korea. She is one of three Japanese members of the
+              South Korean girl group Twice, formed in 2015 by JYP Entertainment
+            </Typography>
+          </CardContent>
+          <CardActions>
+            <Button size="small">Share</Button>
+            <Button size="small">Learn More</Button>
+          </CardActions>
+        </Card>
 
-    <Card sx={{ maxWidth: 300 }}>
-      <CardMedia
-        sx={{ height: 200 }}
-        image="https://i.pinimg.com/564x/a6/0b/77/a60b7777a00978fedb7e37e0f1b2639c.jpg"
-        title="mi-yeon"
-      />
-      <CardContent>
-        <Typography gutterBottom variant="h5" component="div">
-          Choi Mi-yeon
-        </Typography>
-        <Typography variant="body2" color="text.secondary">
-        Cho Mi-yeon, better known mononymously as
-         Miyeon, is a South Korean singer and actress.
-          She is the redirect vocalist of the South Korean girl 
-        group I-dle under Cube Entertainment, and voiced Ahri 
-        in the virtual K-pop girl group K/DA
-        </Typography>
-      </CardContent>
-      <CardActions>
-        <Button size="small">Share</Button>
-        <Button size="small">Learn More</Button>
-      </CardActions>
-    </Card>
+        <Card sx={{ maxWidth: 300 }}>
+          <CardMedia
+            sx={{ height: 200 }}
+            image="https://i.pinimg.com/564x/a6/0b/77/a60b7777a00978fedb7e37e0f1b2639c.jpg"
+            title="mi-yeon"
+          />
+          <CardContent>
+            <Typography gutterBottom variant="h5" component="div">
+              Choi Mi-yeon
+            </Typography>
+            <Typography variant="body2" color="text.secondary">
+              Cho Mi-yeon, better known mononymously as Miyeon, is a South
+              Korean singer and actress. She is the redirect vocalist of the
+              South Korean girl group I-dle under Cube Entertainment, and voiced
+              Ahri in the virtual K-pop girl group K/DA
+            </Typography>
+          </CardContent>
+          <CardActions>
+            <Button size="small">Share</Button>
+            <Button size="small">Learn More</Button>
+          </CardActions>
+        </Card>
 
-    <Card sx={{ maxWidth: 300 }}>
-      <CardMedia
-        sx={{ height: 200 }}
-        image="https://i.pinimg.com/736x/83/8e/b4/838eb4854ae7dce9a06ecb16a167b5cf.jpg"
-        title="lee sung kyung"
-      />
-      <CardContent>
-        <Typography gutterBottom variant="h5" component="div">
-          Lee Sung Kyung
-        </Typography>
-        <Typography variant="body2" color="text.secondary">
-        instagram.com
-Lee Sung-kyung is a South Korean model, actress and singer. She is best known
- for her roles in television series Cheese in the Trap, 
-The Doctors, Weightlifting Fairy Kim Bok-joo, Dr. Romantic 2, and Sh**ting Stars.
-        </Typography>
-      </CardContent>
-      <CardActions>
-        <Button size="small">Share</Button>
-        <Button size="small">Learn More</Button>
-      </CardActions>
-    </Card>
+        <Card sx={{ maxWidth: 300 }}>
+          <CardMedia
+            sx={{ height: 200 }}
+            image="https://i.pinimg.com/736x/83/8e/b4/838eb4854ae7dce9a06ecb16a167b5cf.jpg"
+            title="lee sung kyung"
+          />
+          <CardContent>
+            <Typography gutterBottom variant="h5" component="div">
+              Lee Sung Kyung
+            </Typography>
+            <Typography variant="body2" color="text.secondary">
+              instagram.com Lee Sung-kyung is a South Korean model, actress and
+              singer. She is best known for her roles in television series
+              Cheese in the Trap, The Doctors, Weightlifting Fairy Kim Bok-joo,
+              Dr. Romantic 2, and Sh**ting Stars.
+            </Typography>
+          </CardContent>
+          <CardActions>
+            <Button size="small">Share</Button>
+            <Button size="small">Learn More</Button>
+          </CardActions>
+        </Card>
 
-    <Card sx={{ maxWidth: 300 }}>
-      <CardMedia
-        sx={{ height: 200 }}
-        image="https://i.pinimg.com/736x/6d/a4/be/6da4bebcd992eeeac2c9c725a2a04459.jpg"
-        title="chaewon"
-      />
-      <CardContent>
-        <Typography gutterBottom variant="h5" component="div">
-          Kim Chae-won
-        </Typography>
-        <Typography variant="body2" color="text.secondary">
-        Kim Chae-won is a South Korean singer. She is the leader
-         of South Korean girl group Le Sserafim. She is also a former 
-        member of the reality competition series Produce 48's resulting 
-        girl group Iz*One in 2018. 
-        </Typography>
-      </CardContent>
-      <CardActions>
-        <Button size="small">Share</Button>
-        <Button size="small">Learn More</Button>
-      </CardActions>
-    </Card>
+        <Card sx={{ maxWidth: 300 }}>
+          <CardMedia
+            sx={{ height: 200 }}
+            image="https://i.pinimg.com/736x/6d/a4/be/6da4bebcd992eeeac2c9c725a2a04459.jpg"
+            title="chaewon"
+          />
+          <CardContent>
+            <Typography gutterBottom variant="h5" component="div">
+              Kim Chae-won
+            </Typography>
+            <Typography variant="body2" color="text.secondary">
+              Kim Chae-won is a South Korean singer. She is the leader of South
+              Korean girl group Le Sserafim. She is also a former member of the
+              reality competition series Produce 48's resulting girl group
+              Iz*One in 2018.
+            </Typography>
+          </CardContent>
+          <CardActions>
+            <Button size="small">Share</Button>
+            <Button size="small">Learn More</Button>
+          </CardActions>
+        </Card>
 
-    
-    </div>
+        <Card sx={{ maxWidth: 300 }}>
+          <CardMedia
+            sx={{ height: 200 }}
+            image="https://i.pinimg.com/564x/af/46/4e/af464e1c588286cc35b5e90066f5d6f4.jpg"
+            title="chaeyoung"
+          />
+          <CardContent>
+            <Typography gutterBottom variant="h5" component="div">
+              Son Chaeyoung
+            </Typography>
+            <Typography variant="body2" color="text.secondary">
+              Son Chae-young, known mononymously as Chaeyoung, is a South Korean
+              rapper and singer. She is a member of the South Korean girl group
+              Twice, formed by JYP Entertainment. Chaeyoung decided she wanted
+              to become a singer before joining JYP Entertainment and took dance
+              lessons for over one year.
+            </Typography>
+          </CardContent>
+          <CardActions>
+            <Button size="small">Share</Button>
+            <Button size="small">Learn More</Button>
+          </CardActions>
+        </Card>
 
-    <div className='container-card'>
-      <Card sx={{ maxWidth: 300 }}>
-      <CardMedia
-        sx={{ height: 200 }}
-        image="https://i.pinimg.com/564x/a8/4c/03/a84c037facbceae8def8b08484040ca7.jpg"
-        title="sana"
-      />
-      <CardContent>
-        <Typography gutterBottom variant="h5" component="div">
-          Minatozaki Sana
-        </Typography>
-        <Typography variant="body2" color="text.secondary">
-        Sana Minatozaki, known mononymously as Sana, is a Japanese 
-        singer based in South Korea. She is one of 
-        three Japanese members of the South Korean girl group Twice,
-         formed in 2015 by JYP Entertainment
-        </Typography>
-      </CardContent>
-      <CardActions>
-        <Button size="small">Share</Button>
-        <Button size="small">Learn More</Button>
-      </CardActions>
-    </Card>
+        <Card sx={{ maxWidth: 300 }}>
+          <CardMedia
+            sx={{ height: 200 }}
+            image="https://i.pinimg.com/564x/9d/88/e9/9d88e9c2da836e5ae49c30eca223a9d5.jpg"
+            title="lia"
+          />
+          <CardContent>
+            <Typography gutterBottom variant="h5" component="div">
+              LiA
+            </Typography>
+            <Typography variant="body2" color="text.secondary">
+              Son Chae-young, known mononymously as Chaeyoung, is a South Korean
+              rapper and singer. She is a member of the South Korean girl group
+              Twice, formed by JYP Entertainment. Chaeyoung decided she wanted
+              to become a singer before joining JYP Entertainment and took dance
+              lessons for over one year.
+            </Typography>
+          </CardContent>
+          <CardActions>
+            <Button size="small">Share</Button>
+            <Button size="small">Learn More</Button>
+          </CardActions>
+        </Card>
+      </div>
 
-    <Card sx={{ maxWidth: 300 }}>
-      <CardMedia
-        sx={{ height: 200 }}
-        image="https://i.pinimg.com/564x/a6/0b/77/a60b7777a00978fedb7e37e0f1b2639c.jpg"
-        title="mi-yeon"
-      />
-      <CardContent>
-        <Typography gutterBottom variant="h5" component="div">
-          Choi Mi-yeon
-        </Typography>
-        <Typography variant="body2" color="text.secondary">
-        Cho Mi-yeon, better known mononymously as
-         Miyeon, is a South Korean singer and actress.
-          She is the redirect vocalist of the South Korean girl 
-        group I-dle under Cube Entertainment, and voiced Ahri 
-        in the virtual K-pop girl group K/DA
-        </Typography>
-      </CardContent>
-      <CardActions>
-        <Button size="small">Share</Button>
-        <Button size="small">Learn More</Button>
-      </CardActions>
-    </Card>
+      <div className="container-card">
+        <Card sx={{ maxWidth: 300 }}>
+          <CardMedia
+            sx={{ height: 200 }}
+            image="https://i.pinimg.com/564x/a8/4c/03/a84c037facbceae8def8b08484040ca7.jpg"
+            title="sana"
+          />
+          <CardContent>
+            <Typography gutterBottom variant="h5" component="div">
+              Minatozaki Sana
+            </Typography>
+            <Typography variant="body2" color="text.secondary">
+              Sana Minatozaki, known mononymously as Sana, is a Japanese singer
+              based in South Korea. She is one of three Japanese members of the
+              South Korean girl group Twice, formed in 2015 by JYP Entertainment
+            </Typography>
+          </CardContent>
+          <CardActions>
+            <Button size="small">Share</Button>
+            <Button size="small">Learn More</Button>
+          </CardActions>
+        </Card>
 
-    <Card sx={{ maxWidth: 300 }}>
-      <CardMedia
-        sx={{ height: 200 }}
-        image="https://i.pinimg.com/736x/83/8e/b4/838eb4854ae7dce9a06ecb16a167b5cf.jpg"
-        title="lee sung kyung"
-      />
-      <CardContent>
-        <Typography gutterBottom variant="h5" component="div">
-          Lee Sung Kyung
-        </Typography>
-        <Typography variant="body2" color="text.secondary">
-        instagram.com
-Lee Sung-kyung is a South Korean model, actress and singer. She is best known
- for her roles in television series Cheese in the Trap, 
-The Doctors, Weightlifting Fairy Kim Bok-joo, Dr. Romantic 2, and Sh**ting Stars.
-        </Typography>
-      </CardContent>
-      <CardActions>
-        <Button size="small">Share</Button>
-        <Button size="small">Learn More</Button>
-      </CardActions>
-    </Card>
+        <Card sx={{ maxWidth: 300 }}>
+          <CardMedia
+            sx={{ height: 200 }}
+            image="https://i.pinimg.com/564x/a6/0b/77/a60b7777a00978fedb7e37e0f1b2639c.jpg"
+            title="mi-yeon"
+          />
+          <CardContent>
+            <Typography gutterBottom variant="h5" component="div">
+              Choi Mi-yeon
+            </Typography>
+            <Typography variant="body2" color="text.secondary">
+              Cho Mi-yeon, better known mononymously as Miyeon, is a South
+              Korean singer and actress. She is the redirect vocalist of the
+              South Korean girl group I-dle under Cube Entertainment, and voiced
+              Ahri in the virtual K-pop girl group K/DA
+            </Typography>
+          </CardContent>
+          <CardActions>
+            <Button size="small">Share</Button>
+            <Button size="small">Learn More</Button>
+          </CardActions>
+        </Card>
 
-    <Card sx={{ maxWidth: 300 }}>
-      <CardMedia
-        sx={{ height: 200 }}
-        image="https://i.pinimg.com/736x/6d/a4/be/6da4bebcd992eeeac2c9c725a2a04459.jpg"
-        title="chaewon"
-      />
-      <CardContent>
-        <Typography gutterBottom variant="h5" component="div">
-          Kim Chae-won
-        </Typography>
-        <Typography variant="body2" color="text.secondary">
-        Kim Chae-won is a South Korean singer. She is the leader
-         of South Korean girl group Le Sserafim. She is also a former 
-        member of the reality competition series Produce 48's resulting 
-        girl group Iz*One in 2018. 
-        </Typography>
-      </CardContent>
-      <CardActions>
-        <Button size="small">Share</Button>
-        <Button size="small">Learn More</Button>
-      </CardActions>
-    </Card>
+        <Card sx={{ maxWidth: 300 }}>
+          <CardMedia
+            sx={{ height: 200 }}
+            image="https://i.pinimg.com/736x/83/8e/b4/838eb4854ae7dce9a06ecb16a167b5cf.jpg"
+            title="lee sung kyung"
+          />
+          <CardContent>
+            <Typography gutterBottom variant="h5" component="div">
+              Lee Sung Kyung
+            </Typography>
+            <Typography variant="body2" color="text.secondary">
+              instagram.com Lee Sung-kyung is a South Korean model, actress and
+              singer. She is best known for her roles in television series
+              Cheese in the Trap, The Doctors, Weightlifting Fairy Kim Bok-joo,
+              Dr. Romantic 2, and Sh**ting Stars.
+            </Typography>
+          </CardContent>
+          <CardActions>
+            <Button size="small">Share</Button>
+            <Button size="small">Learn More</Button>
+          </CardActions>
+        </Card>
 
-    
-    </div>
-  </>
-  )
+        <Card sx={{ maxWidth: 300 }}>
+          <CardMedia
+            sx={{ height: 200 }}
+            image="https://i.pinimg.com/736x/6d/a4/be/6da4bebcd992eeeac2c9c725a2a04459.jpg"
+            title="chaewon"
+          />
+          <CardContent>
+            <Typography gutterBottom variant="h5" component="div">
+              Kim Chae-won
+            </Typography>
+            <Typography variant="body2" color="text.secondary">
+              Kim Chae-won is a South Korean singer. She is the leader of South
+              Korean girl group Le Sserafim. She is also a former member of the
+              reality competition series Produce 48's resulting girl group
+              Iz*One in 2018.
+            </Typography>
+          </CardContent>
+          <CardActions>
+            <Button size="small">Share</Button>
+            <Button size="small">Learn More</Button>
+          </CardActions>
+        </Card>
+
+        <Card sx={{ maxWidth: 300 }}>
+          <CardMedia
+            sx={{ height: 200 }}
+            image="https://i.pinimg.com/564x/af/46/4e/af464e1c588286cc35b5e90066f5d6f4.jpg"
+            title="chaeyoung"
+          />
+          <CardContent>
+            <Typography gutterBottom variant="h5" component="div">
+              Son Chaeyoung
+            </Typography>
+            <Typography variant="body2" color="text.secondary">
+              Son Chae-young, known mononymously as Chaeyoung, is a South Korean
+              rapper and singer. She is a member of the South Korean girl group
+              Twice, formed by JYP Entertainment. Chaeyoung decided she wanted
+              to become a singer before joining JYP Entertainment and took dance
+              lessons for over one year.
+            </Typography>
+          </CardContent>
+          <CardActions>
+            <Button size="small">Share</Button>
+            <Button size="small">Learn More</Button>
+          </CardActions>
+        </Card>
+
+        <Card sx={{ maxWidth: 300 }}>
+          <CardMedia
+            sx={{ height: 200 }}
+            image="https://i.pinimg.com/564x/9d/88/e9/9d88e9c2da836e5ae49c30eca223a9d5.jpg"
+            title="lia"
+          />
+          <CardContent>
+            <Typography gutterBottom variant="h5" component="div">
+              LiA
+            </Typography>
+            <Typography variant="body2" color="text.secondary">
+              Son Chae-young, known mononymously as Chaeyoung, is a South Korean
+              rapper and singer. She is a member of the South Korean girl group
+              Twice, formed by JYP Entertainment. Chaeyoung decided she wanted
+              to become a singer before joining JYP Entertainment and took dance
+              lessons for over one year.
+            </Typography>
+          </CardContent>
+          <CardActions>
+            <Button size="small">Share</Button>
+            <Button size="small">Learn More</Button>
+          </CardActions>
+        </Card>
+      </div>
+    </>
+  );
 }
 
-export default Main
+export default Main;
